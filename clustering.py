2145cf370878f57acbb959abf7f58b2a40a59b1d
shashankgargnyu <sg4437@nyu.edu>
2017-05-03 23:28:42 -0400

    making some methods private

--- clustering.py
+++ clustering.py
@@ -6,13 +6,13 @@ class KHMean:
     def __init__(self):
         pass
 
-    def get_distance(self, x, centers):
+    def _get_distance(self, x, centers):
         dist = []
         for i in range(len(centers)):
             dist.append(np.linalg.norm(np.array(centers[i]) - np.array(x)))
         return dist
 
-    def get_weight(self, d):
+    def _get_weight(self, d):
         return np.power(d, 3) * (np.power(sum(1./np.power(d, 2)), 2))
 
     def train(self, rdd, k, maxIterations=100):
@@ -33,10 +33,10 @@ class KHMean:
         print ('initial centers')
         print (centers)
         for i in range(maxIterations):
-            d = rdd.map(lambda x: [x, self.get_distance(x, centers)]).map(lambda x: [x[0], np.array(x[1])+1e-6])\
+            d = rdd.map(lambda x: [x, self._get_distance(x, centers)]).map(lambda x: [x[0], np.array(x[1])+1e-6])\
                 .map(lambda x: [x[0], x[1].tolist()])
             d_indexed = d.map(lambda x: [x[0], x[1], x[1].index(min(x[1]))])
-            q_indexed = d_indexed.map(lambda x: [x[0], self.get_weight(np.array(x[1])), x[2]])
+            q_indexed = d_indexed.map(lambda x: [x[0], self._get_weight(np.array(x[1])), x[2]])
             for l in range(k):
                 m = q_indexed.filter(lambda x: x[2] == l).map(lambda x: [x[0], x[1].tolist(), x[2]])\
                     .map(lambda x: [x[0], x[1][l] * np.array(x[0]), x[1][l], x[2]])
